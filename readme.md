cca560c62a74e3b76335c78362b405f2968869f9
OdinWattz <145436065+OdinWattz@users.noreply.github.com>
2024-03-11 11:24:34 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Procenten-rekentool
+
---
+++ script.js
@@ -0,0 +1,11 @@
+function solveProblem() {
+    alert('I am a problem solver');
+}
+
+function checkSolveButton(){
+    let nieuw = document.getElementById("nieuw");
+    let oud = document.getElementById("oud");
+    let factor = document.getElementById("percentage");
+
+
+}
